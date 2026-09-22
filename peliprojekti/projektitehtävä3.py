20941bb54ff3d958af2d3cc5072c1b8eb10f8ae7
Tubees <98246435+Tubees@users.noreply.github.com>
2026-09-22 22:27:30 +0300

    projektitehtävä3

---
+++ peliprojekti/projektitehtävä3.py
@@ -0,0 +1,39 @@
+nimi = input("Syötä nimi: ")
+ikä = int(input("Syötä ikä: "))
+
+lista = []
+
+def aloita():
+    print("\nPeli alkaa")
+    print("\nLisää esineitä reppuun(Lopeta syötämällä tyhjä merkkijono)")
+    while True:
+        x = input("Syötä esine: ")
+        lista.append(x)
+        if x == "":
+            break
+
+def listaa():
+    print("\nEsineet repussa:")
+    for x in lista:
+        print(x)
+
+def lopeta():
+    print("Peli suljetaan")
+    quit()
+
+if ikä < 12:
+    print("Peli kielletty alaikäisiltä. Peli suljetaan...")
+    quit()
+else:
+    while True:
+        print("\nHei " + nimi + "!\n1. Aloita Peli\n2. Listaa esineet\n")
+        valinta = input("Valitse toiminto(1-2) tai syötä 'lopeta': ")
+        if valinta == "1":
+            aloita()
+        elif valinta == "2":
+            listaa()
+        elif valinta == "lopeta":
+            lopeta()
+
+
+
